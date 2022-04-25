123b2f68ae61c8e8266ae4b327c9168fc3436087
Yoyo Yang <yoyo.truly@gmail.com>
2022-04-24 23:08:04 -0400

    Refactor main function using utility module

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
 const { nextISSTimesForMyLocation } = require("./iss");
+const { printFlyOverTimes } = require("./utilities");
 
 nextISSTimesForMyLocation((error, flyOverTimes) => {
   if (error) {
     return console.log(`It didn't work! ${error}`);
   }
-  for (const { risetime, duration } of flyOverTimes) {
-    const datetime = new Date(0);
-    datetime.setUTCSeconds(risetime);
-    console.log(`Next pass at ${datetime} for ${Math.round(duration / 60)} minutes`);
-  }
+  
+  printFlyOverTimes(flyOverTimes);
 });
